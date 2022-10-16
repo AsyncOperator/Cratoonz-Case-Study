a58dd2719db57eb3c3691e9addc80e3805c1c402
Nihat Hakan Altıparmak <nhakan6parmak@gmail.com>
2022-10-16 22:32:55 +0300

    Refactoring swiper script

--- Assets/_Scripts/Core/Swiper.cs
+++ Assets/_Scripts/Core/Swiper.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Core.Board;
 using Grid = Core.Grid<Tile>;
 using AsyncOperator.Helpers;
 
@@ -22,19 +23,24 @@ namespace Core {
         } );
 
         [Range( 10f, 40f ), SerializeField] private float angleThreshold;
+        [SerializeField] private Matcher matcher;
 
         private Tile selectedTile;
         private Vector2 touchStartPosition;
         private Vector2 touchEndPosition;
 
-        public Grid Grid { get; set; }
+        private Grid grid;
 
         private void OnEnable() {
+            FindObjectOfType<BoardCreator>().OnBoardCreated += ( g ) => grid = g;
+
             InputManager.OnTouchStartPosition += InputManager_OnTouchStartPosition;
             InputManager.OnTouchCancelPosition += InputManager_OnTouchCancelPosition;
         }
 
         private void OnDisable() {
+            FindObjectOfType<BoardCreator>().OnBoardCreated -= ( g ) => grid = g;
+
             InputManager.OnTouchStartPosition -= InputManager_OnTouchStartPosition;
             InputManager.OnTouchCancelPosition -= InputManager_OnTouchCancelPosition;
         }
@@ -46,7 +52,7 @@ namespace Core {
 
             // If ray hit something then try get tile component from the hit object, otherwise set selectedTile field to null
             if ( hit.transform != null ) {
-                selectedTile = hit.transform.TryGetComponent( out Tile tile ) ? tile : null; 
+                selectedTile = hit.transform.TryGetComponent( out Tile tile ) ? tile : null;
             }
             // If ray did not hit anything
             else {
@@ -87,70 +93,58 @@ namespace Core {
                 Tile targetTile = null;
 
                 int x, y;
-                Grid.GetXY( selectedTile.transform.position, out x, out y );
+                grid.GetXY( selectedTile.transform.position, out x, out y );
 
                 switch ( swipeDirectionType ) {
                     case DirectionType.Right:
-                        targetTile = Grid.RightNeighbour( x, y );
+                        targetTile = grid.RightNeighbour( x, y );
                         if ( targetTile != null ) {
                             if ( targetTile.Drop != null ) {
-                                Drop selectedTileDrop = selectedTile.Drop;
-                                Drop targetTileDrop = targetTile.Drop;
-
-                                selectedTileDrop.transform.SetParent( targetTile.transform, false );
-                                targetTileDrop.transform.SetParent( selectedTile.transform, false );
-
-                                selectedTile.Drop = targetTileDrop;
-                                targetTile.Drop = selectedTileDrop;
+                                Swap( targetTile );
                             }
                         }
                         break;
                     case DirectionType.Left:
-                        targetTile = Grid.LeftNeighbour( x, y );
+                        targetTile = grid.LeftNeighbour( x, y );
                         if ( targetTile != null ) {
                             if ( targetTile.Drop != null ) {
-                                Drop selectedTileDrop = selectedTile.Drop;
-                                Drop targetTileDrop = targetTile.Drop;
-
-                                selectedTileDrop.transform.SetParent( targetTile.transform, false );
-                                targetTileDrop.transform.SetParent( selectedTile.transform, false );
-
-                                selectedTile.Drop = targetTileDrop;
-                                targetTile.Drop = selectedTileDrop;
+                                Swap( targetTile );
                             }
                         }
                         break;
                     case DirectionType.Up:
-                        targetTile = Grid.UpNeighbour( x, y );
+                        targetTile = grid.UpNeighbour( x, y );
                         if ( targetTile != null ) {
                             if ( targetTile.Drop != null ) {
-                                Drop selectedTileDrop = selectedTile.Drop;
-                                Drop targetTileDrop = targetTile.Drop;
-
-                                selectedTileDrop.transform.SetParent( targetTile.transform, false );
-                                targetTileDrop.transform.SetParent( selectedTile.transform, false );
-
-                                selectedTile.Drop = targetTileDrop;
-                                targetTile.Drop = selectedTileDrop;
+                                Swap( targetTile );
                             }
                         }
                         break;
                     case DirectionType.Down:
-                        targetTile = Grid.DownNeighbour( x, y );
+                        targetTile = grid.DownNeighbour( x, y );
                         if ( targetTile != null ) {
                             if ( targetTile.Drop != null ) {
-                                Drop selectedTileDrop = selectedTile.Drop;
-                                Drop targetTileDrop = targetTile.Drop;
-
-                                selectedTileDrop.transform.SetParent( targetTile.transform, false );
-                                targetTileDrop.transform.SetParent( selectedTile.transform, false );
-
-                                selectedTile.Drop = targetTileDrop;
-                                targetTile.Drop = selectedTileDrop;
+                                Swap( targetTile );
                             }
                         }
                         break;
                 }
+            }
+
+            void Swap( Tile targetTile ) {
+                Drop selectedTileDrop = selectedTile.Drop;
+                Drop targetTileDrop = targetTile.Drop;
+
+                selectedTileDrop.transform.SetParent( targetTile.transform, false );
+                targetTileDrop.transform.SetParent( selectedTile.transform, false );
+
+                selectedTile.Drop = targetTileDrop;
+                targetTile.Drop = selectedTileDrop;
+
+                Vector2Int firstRowColumn = grid.GetXY( selectedTile.transform.position );
+                Vector2Int secondRowColumn = grid.GetXY( targetTile.transform.position );
+
+                matcher.Match( firstRowColumn, secondRowColumn );
             }
         }
     }
