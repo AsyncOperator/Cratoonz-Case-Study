b146cce8fdfb96e1f314e8f26a03860bf3519346
Nihat Hakan Altıparmak <nhakan6parmak@gmail.com>
2022-10-16 12:16:45 +0300

    ~Documentation

--- Assets/_Scripts/Core/BoardSetter.cs
+++ Assets/_Scripts/Core/BoardSetter.cs
@@ -9,11 +9,15 @@ namespace Core.Board {
         [SerializeField] private DropSO[] dropSOs;
 
         public void SetBoard( Grid<Tile> grid, int width, int height ) {
+            // local caching
             Tile downTile = null;
             Tile leftTile = null;
 
             for ( int x = 0 ; x < width ; x++ ) {
                 for ( int y = 0 ; y < height ; y++ ) {
+
+                    // Since we are moving from left to right (row increment) and bottom to top (column increment),
+                    // we can just check left and bottom neighbour of current tile to make decision of which dropData can be set for the current tile
                     leftTile = grid.LeftNeighbour( x, y );
                     downTile = grid.DownNeighbour( x, y );
 
@@ -23,10 +27,11 @@ namespace Core.Board {
                     var dropInstance = Instantiate<Drop>( dropPf );
                     dropInstance.transform.SetParent( tileInstance.transform, false );
 
-                    var availableDropSOs = dropSOs.Where( drop => drop != downTile?.Drop.DropData && drop != leftTile?.Drop.DropData );
+                    var availableDropSOs = dropSOs.Where( drop => drop != downTile?.Drop?.DropData && drop != leftTile?.Drop?.DropData );
                     dropInstance.DropData = availableDropSOs.ElementAt( Random.Range( 0, availableDropSOs.Count() ) );
                     tileInstance.Drop = dropInstance;
 
+                    // Finally, set grid value by generated tile
                     grid.SetValue( x, y, tileInstance );
                 }
             }
