aa8d962e74d57a0d684c19f63d49ad0de5a8ddd2
Nihat Hakan Altıparmak <nhakan6parmak@gmail.com>
2022-10-17 18:47:23 +0300

    DroMover script responsible for handling smooth movement via tween

---
+++ Assets/_Scripts/Core/DropMover.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+
+public sealed class DropMover : MonoBehaviour {
+    [SerializeField] private float swapDuration;
+
+    public IEnumerator SwapTiles( Tile t1, Tile t2 ) {
+        Drop t1drop = t1.Drop;
+        Drop t2drop = t2.Drop;
+
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Join( t1drop.transform.DOMove( t2.transform.position, swapDuration ) );
+        sequence.Join( t2drop.transform.DOMove( t1.transform.position, swapDuration ) );
+
+        yield return sequence.WaitForCompletion();
+
+        t1drop.transform.SetParent( t2.transform, true );
+        t2drop.transform.SetParent( t1.transform, true );
+
+        t1.Drop = t2drop;
+        t2.Drop = t1drop;
+    }
+}
